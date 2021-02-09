060ad9bc5ceb8bcf700d506046efece3e021390e
Matthew Horton <mkhorton@users.noreply.github.com>
2021-02-08 20:43:11 -0800

    Update comments

--- pymatgen-core/prepare_core.py
+++ pymatgen-core/prepare_core.py
@@ -1,9 +1,9 @@
 import shutil
 import os
 
-# Copy pymatgen core folders from parent
-# (This is probably unnecessary, just for prototyping)
 shutil.copytree("../pymatgen/core", "pymatgen/core")
 shutil.copytree("../pymatgen/util", "pymatgen/util")
+
+# remove __init__.py to make packages implicit namespace packages
 os.remove("pymatgen/core/__init__.py")
 os.remove("pymatgen/util/__init__.py")
